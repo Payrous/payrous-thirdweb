d59dcd7ed9dfe1a8bc7a501e6a3317d044f84220
Labi-Joy <joyeden325@gmail.com>
2025-03-14 04:05:22 -0700

    fixes, edit

--- src/app/dashboard/dashboard/page.tsx
+++ src/app/dashboard/dashboard/page.tsx
@@ -9,9 +9,9 @@ const Dashboard = () => {
   return (
     <div className='flex flex-col pt-4 px-6 lg:pt-10 lg:px-10 text-colors-BlueGray'>
       {/* fund cards */}
-      <div className="flex flex-col md:flex-row gap-6">
+      <div className="flex flex-col md:flex-row gap-6 w-full md:max-w-[69%] justify-between">
         {/* income */}
-        <div className="w-full h-[136px] md:w-80  border border-colors-LightGray rounded-xl">
+        <div className="w-full h-[136px] border border-colors-LightGray rounded-xl">
           <div className="flex justify-between p-6">
             <div className="flex gap-3">
               <Image src={green} alt="income icon" />
@@ -26,7 +26,7 @@ const Dashboard = () => {
           </div>
         </div>
         {/* expenditure */}
-        <div className="w-full h-[136px] md:w-80  border border-colors-LightGray rounded-xl">
+        <div className="w-full h-[136px] border border-colors-LightGray rounded-xl">
           <div className="flex justify-between p-6">
             <div className="flex gap-3">
               <Image src={red} alt="income icon" />
@@ -40,8 +40,24 @@ const Dashboard = () => {
             </div>
           </div>
         </div>
-        {/* org select */}
-        <OverveiwOrgCard />
+      </div>
+
+      <div className="flex flex-col md:flex-row-reverse md:items-start md:justify-between gap-6 mt-6">
+        <div className="mt-0 md:-mt-40">
+          <OverveiwOrgCard />
+        </div>
+        <div className="w-full h-64 md:col-span-2 border bg-colors-WhiteSmoke rounded-xl mt-6 md:mt-5">
+
+        </div>
+      </div>
+
+      <div className="flex flex-col md:flex-row gap-6 mt-6">
+        <div className="w-full h-[430px] md:col-span-2 md:w-[69%] border bg-colors-WhiteSmoke rounded-xl">
+
+        </div>
+        <div className="w-full h-[430px] md:w-[350px] border bg-colors-WhiteSmoke rounded-xl">
+
+        </div>
       </div>
 
     </div>
